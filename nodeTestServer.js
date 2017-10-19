b426cf64fae8401389cf35578d671dcc6b933615
mclaurinpd <mclaurinpd@gmail.com>
2017-10-18 20:02:54 -0400

    modified json data to have unique object identifiers

--- nodeTestServer.js
+++ nodeTestServer.js
@@ -8,13 +8,13 @@ app.get('/temp/latest', function (req, res) {
        var latest = table.device1.timestamp;
        var index = 1;
 	   for (var i = 1; i <= Object.keys(table).length; i++) {
-			var timeCheck = table['device'+i].timestamp
+			var timeCheck = table['reading'+i].timestamp
 			if (timeCheck > latest) {
 				index = i;
 				latest = timeCheck;
 			}	
 	   }
-	   var device = table['device'+index];
+	   var reading = table['reading'+index];
 	   res.end("Most recent submission since UNIX epoch: \n\n" + "Device Id: " + device.device_id + "\nTimestamp: " + device.timestamp + "\nTemperature: " + device.temperature);
    });
 })
@@ -25,13 +25,13 @@ app.get('/temp/highest', function (req, res) {
        var highest = table.device1.temperature;
        var index = 1;
 	   for (var i = 1; i <= Object.keys(table).length; i++) {
-			var highCheck = table['device'+i].temperature
+			var highCheck = table['reading'+i].temperature
 			if (highCheck > highest) {
 				index = i;
 				highest = highCheck;
 			}	
 	   }
-	   var device = table['device'+index];
+	   var reading = table['reading'+index];
 	   res.end("Highest temperature: \n\n" + "Device Id: " + device.device_id + "\nTimestamp: " + device.timestamp + "\nTemperature: " + device.temperature);
    });
 })
@@ -42,13 +42,13 @@ app.get('/temp/lowest', function (req, res) {
        var lowest = table.device1.temperature;
        var index = 1;
 	   for (var i = 1; i <= Object.keys(table).length; i++) {
-			var lowCheck = table['device'+i].temperature
+			var lowCheck = table['reading'+i].temperature
 			if (lowCheck < lowest) {
 				index = i;
 				lowest = lowCheck;
 			}	
 	   }
-	   var device = table['device'+index];
+	   var reading = table['reading'+index];
 	   res.end("Lowest temperature: \n\n" + "Device Id: " + device.device_id + "\nTimestamp: " + device.timestamp + "\nTemperature: " + device.temperature);
    });
 })
@@ -59,10 +59,28 @@ app.get('/temp/average', function (req, res) {
        var average = table.device1.temperature;
 	   var tableLength = Object.keys(table).length
 	   for (var i = 1; i <= tableLength; i++) {
-			average = average + table['device'+i].temperature;	
+			average = average + table['reading'+i].temperature;	
 	   }
 	   res.end("Average temperature: " + (average / tableLength));
    });
+})
+
+app.get('/temp/:id', function (req, res) {
+   fs.readFile( __dirname + "/" + "deviceData.json", 'utf8', function (err, data) {
+	   var table = JSON.parse(data);
+	   var deviceName = req.params.id;
+	   var string = "";
+	   for (var i = 1; i <= Object.keys(table).length; i++) {
+	   		var obj = table['reading'+i];
+	   		console.log(obj.device_id);
+	   		if (obj.device_id == deviceName) {
+	   			console.log("asf");
+	   			string += 'Temperature: ' + obj.temperature + "\n";
+	   			string += 'Timestamp: ' + obj.timestamp + "\n\n";
+	   		}
+	   }
+	   res.end(string);
+	});
 })
 
 var server = app.listen(8081, function () {
